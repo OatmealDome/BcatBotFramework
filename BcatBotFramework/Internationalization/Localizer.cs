87eb7e3822ff2807bd1f38c690822fb427818614
OatmealDome <theoatmealdome@gmail.com>
2019-03-29 02:26:57 -0400

    Localizer: Implement Initialize and Localize

--- BcatBotFramework/Internationalization/Localizer.cs
+++ BcatBotFramework/Internationalization/Localizer.cs
@@ -1,18 +1,41 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
+using BcatBotFramework.Core.Config;
+using Newtonsoft.Json;
 using Nintendo.Bcat;
 
 namespace BcatBotFramework.Internationalization
 {
     public class Localizer
     {
+        private static Dictionary<Language, Dictionary<string, string>> Localizations;
         private static bool Initialized = false;
 
         public static void Initialize()
         {
+            Localizations = new Dictionary<Language, Dictionary<string, string>>();
 
+            // Populate the Dictionary for each Languages
+            foreach (Language language in LanguageExtensions.GetAllLanguages())
+            {
+                // Create the localization file path
+                string path = Path.Combine(Configuration.LoadedConfiguration.LocalizerConfig.LocalizationsDirectory, language.GetCode() + ".json");
+
+                // Check if the file exists
+                if (File.Exists(path))
+                {
+                    // Load the file
+                    Localizations[language] = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
+                }
+                else
+                {
+                    // Create a new blank Dictionary
+                    Localizations[language] = new Dictionary<string, string>();
+                }
+            }
         }
 
         public static void Dispose()
@@ -22,8 +45,22 @@ namespace BcatBotFramework.Internationalization
 
         public static string Localize(string key, Language language)
         {
+            // Try to get the localization
+            if (Localizations[language].TryGetValue(key, out string value))
+            {
+                // Return the value
+                return value;
+            }
+
+            // Try to get the localization in en-US
+            /*if (Localizations[Language.EnglishUS].TryGetValue(key, out string value))
+            {
+                // Return the value
+                return value;
+            }*/
+
             // TODO: Real localization
-            return key;
+            return $"String not found ({key}";
         }
 
         public static Dictionary<Language, string> LocalizeToAllLanguages(string key)
