674477710857a5cf03818154b1b73c3633069a02
OatmealDome <theoatmealdome@gmail.com>
2019-07-29 20:32:28 -0400

    Discord: Add NotificationsSettings to replace GuildSettings

---
+++ BcatBotFramework/Social/Discord/Notifications/NotificationsSettings.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BcatBotFramework.Social.Discord.Notifications
+{
+    public class NotificationsSettings
+    {
+        [JsonProperty("Guild")]
+        public ulong GuildId
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("Channel")]
+        public ulong ChannelId
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("Data")]
+        private Dictionary<string, dynamic> Data;
+
+        public NotificationsSettings(ulong guild, ulong channel)
+        {
+            GuildId = guild;
+            ChannelId = channel;
+            Data = new Dictionary<string, dynamic>();
+        }
+
+        public dynamic GetSetting(string name)
+        {
+            if (Data.TryGetValue(name, out dynamic val))
+            {
+                return val;
+            }
+
+            return null;
+        }
+
+        public void SetSetting(string name, dynamic val)
+        {
+            Data[name] = val;
+        }
+
+    }
+
+}
