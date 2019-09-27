af93bf13392da369f16b60ac4b363e499c6d1b48
OatmealDome <julian@oatmealdome.me>
2019-09-26 22:00:03 -0400

    Nintendo: Add enum for Nintendo platforms

---
+++ Nintendo/NintendoPlatform.cs
@@ -0,0 +1,16 @@
+namespace Nintendo
+{
+    public enum NintendoPlatform : int
+    {
+        // NetInfo order
+        DS = 0,
+        Wii,
+        ThreeDS,
+        WiiU,
+        Switch,
+        iOS,
+        Android,
+        Web,
+        Other
+    }
+}
