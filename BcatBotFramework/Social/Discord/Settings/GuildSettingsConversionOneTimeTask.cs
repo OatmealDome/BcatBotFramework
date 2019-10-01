c31c03e413919eda0124c256f53f53ca1bbfaadb
OatmealDome <julian@oatmealdome.me>
2019-10-01 02:28:40 -0400

    GuildSettingsConversionOneTimeTask: Add ability to perform post-conversion tasks

--- BcatBotFramework/Social/Discord/Settings/GuildSettingsConversionOneTimeTask.cs
+++ BcatBotFramework/Social/Discord/Settings/GuildSettingsConversionOneTimeTask.cs
@@ -4,9 +4,9 @@ using BcatBotFramework.Core.Config;
 
 namespace BcatBotFramework.Social.Discord.Settings
 {
-    public class GuildSettingsConversionOneTimeTask : OneTimeTask
+    public abstract class GuildSettingsConversionOneTimeTask : OneTimeTask
     {
-        protected override Task Run()
+        protected override async Task Run()
         {
             // Loop over every GuildSettings
             foreach (GuildSettings guildSettings in Configuration.LoadedConfiguration.DiscordConfig.GuildSettings)
@@ -20,10 +20,13 @@ namespace BcatBotFramework.Social.Discord.Settings
 
                 // Create a new DynamicSettingsData instance and add it to the GuildSettings
                 guildSettings.ChannelSettings.TryAdd(guildSettings.TargetChannelId, channelSettings);
-            }
 
-            return Task.FromResult(0);
+                // Do post conversion things
+                await DoPostConversionThings(guildSettings, channelSettings);
+            }
         }
+
+        protected abstract Task DoPostConversionThings(GuildSettings guildSettings, DynamicSettingsData channelSettings);
 
     }
 }
